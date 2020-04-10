176cabb14d14f719cf21e84b784cd1d92c2d934e
teodoraargintaru <teodora.argintaru@outlook.com>
2020-04-10 20:33:18 +0300

    defined attacked matrix

--- engine.h
+++ engine.h
@@ -2,8 +2,8 @@
 // Created by Teodora Argintaru on 09/03/2020.
 //
 
-#ifndef LADYBUGS_CHESS_ENGINE_ENGINE_H
-#define LADYBUGS_CHESS_ENGINE_ENGINE_H
+#ifndef LADYBUGS_ENGINE_H
+#define LADYBUGS_ENGINE_H
 
 #include "codifications.h"
 
@@ -13,6 +13,34 @@ inline unordered_map<int, int> positions;
 inline bool force = false;
 inline char lastMove[5];
 inline int colorToMove = WHITE, engineColor = NONE;
+
+inline vector<int> knightMovesX {2, 1, -1, -2, -2, -1, 1, 2};
+inline vector<int> knightMovesY {1, 2, 2, 1, -1, -2, -2, -1};
+inline int KNIGHT_MOVES = 8;
+
+inline vector<int> pawnMovesX {1, -1, 0};
+inline int PAWN_MOVES = 3;
+
+inline vector<int> kingMovesX {-1, 0, 1, -1, 1, -1, 0, 1};
+inline vector<int> kingMovesY {-1, -1, -1, 0, 0, 1, 1, 1};
+inline int KING_MOVES = 8;
+
+inline vector< vector< vector< vector<int> > > > attacked
+    (2, vector< vector< vector< int> > >
+        (9, vector< vector < int> >
+                (9, vector <int> (0, 0))));
+
+// calls insertRookMoves/insertBishopMoves or both
+void generateMoveVectors(int piece, vector<int> &movesX, vector<int> &movesY);
+
+// returns a vector of strings -> possible moves that can be written to stdout
+// for each piece
+vector<string> getPieceMoves(int piece, int movesNr,
+                             vector<int> &movesX, vector<int> &movesY);
+
+// functions that generate moves vectors for bishop/rooks
+void insertRookMoves(int x, vector<int> &movesX, vector<int> &movesY);
+void insertBishopMoves(int x, vector<int> &movesX, vector<int>&movesY);
 
 // translates position from board[line][col] to positions map value
 void translatePosition(int line, int col, int piece);
